097f97a2fa8f5664a7de701cee3a61673756d27d
Marlena Gorka <marlenagorka.mg@gmail.com>
2023-03-13 11:25:07 +0100

    fix flask & login form

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
